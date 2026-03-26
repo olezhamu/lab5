7be8a39acd47cce6a919a2d4be6c39ad6c06a4f5
Oleg <mushkevich.oleg@gmail.com>
2026-03-26 14:22:30 +0300

    missed this in pre last commit about commands

---
+++ src/main/java/commands/ExecuteScript.java
@@ -0,0 +1,28 @@
+//считать и исполнить скрипт из указанного файла. В скрипте содержатся команды в таком же виде,
+// в котором их вводит пользователь в интерактивном режиме.
+
+package commands;
+
+import managers.CommandManager;
+import managers.FileManager;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ExecuteScript implements Command{
+    @Override
+    public String execute(String argument) throws IOException {
+        String fileName = argument;
+        List<String> commandsToRun = new FileManager().readCommands(fileName);
+        for (String commandToRun : commandsToRun) {
+            new CommandManager().executeCommand(commandToRun);
+        }
+        String output = "script successfully executed";
+        return output;
+    }
+
+    @Override
+    public String toString() {
+        return "executes script from file";
+    }
+}
